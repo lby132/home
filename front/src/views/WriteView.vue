2928513be0557418d0190fcae499ce4bc27d5359
lby132 <quddbscjswow@naver.com>
2022-11-09 01:15:08 +0900

    vue.js를 이용한 글 작성, 리스트, 수정, cors문제 해결

--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
 import {ref} from "vue";
-
-import axios from 'axios';
+import axios from "axios";
+import { useRouter } from "vue-router";
 
 const title = ref("")
 const content = ref("")
 
+const router = useRouter();
+
 const write = function() {
-    axios.post("http://localhost:8080/posts", {
-        title: title.value,
-        content: content.value
+    axios
+        .post("/api/posts", {
+         title: title.value,
+         content: content.value
+    })
+    .then(() => {
+      router.replace({ name: "home" });
     });
-}
+};
 </script>
 
 <template>
