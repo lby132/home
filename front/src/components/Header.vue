63a178deb8fe21a2d317676438eaaeb4df09a268
lby132 <quddbscjswow@naver.com>
2022-11-09 23:50:20 +0900

    vue.js 화면 꾸미기

---
+++ front/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+ <el-header class="header">
+<!--        <RouterLink to="/">Home</RouterLink> -->
+<!--        <RouterLink to="/write">글작성</RouterLink> -->
+   <el-menu mode="horizontal">
+     <el-menu-item index="1">Home</el-menu-item>
+     <el-menu-item index="2">글 작성</el-menu-item>
+   </el-menu>
+  </el-header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.header {
+  padding: 0;
+  height: 60px;
+}
+</style>
